a075b1e08ad6027a8d4f97130a4f10bc7a8c8c5d
David Barraza <dbcomps@mac.com>
2022-08-24 22:20:27 -0500

    displaying 4 days and set of data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#cityname");
 var citySearchTerm = document.querySelector("#city-search-term")
-var weatherContainerEl = document.querySelector("#weather-container");
+var weather1ContainerEl = document.querySelector("#day1-weather-container");
+var weather1TempEl = document.querySelector("#weather1-data-temp");
+var weather1HumidityEl = document.querySelector("#weather1-data-humidity");
+var weather1WindEl = document.querySelector("#weather1-data-wind");
 
-console.log(cityFormEl);
-console.log(cityInputEl);
+var weather2ContainerEl = document.querySelector("#day2-weather-container");
+var weather2TempEl = document.querySelector("#weather2-data-temp");
+var weather2HumidityEl = document.querySelector("#weather2-data-humidity");
+var weather2WindEl = document.querySelector("#weather2-data-wind");
+
+var weather3ContainerEl = document.querySelector("#day3-weather-container");
+var weather3TempEl = document.querySelector("#weather3-data-temp");
+var weather3HumidityEl = document.querySelector("#weather3-data-humidity");
+var weather3WindEl = document.querySelector("#weather3-data-wind");
+
+var weather4ContainerEl = document.querySelector("#day4-weather-container");
+var weather4TempEl = document.querySelector("#weather4-data-temp");
+var weather4HumidityEl = document.querySelector("#weather4-data-humidity");
+var weather4WindEl = document.querySelector("#weather4-data-wind");
 
 var formSubmitHandler = function(event) {
 	event.preventDefault();
@@ -24,7 +39,7 @@ var formSubmitHandler = function(event) {
 
 var getCityWeatherData = function(cityname) {
 	// format the openweather api url
-	var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&APPID=e3e03c31e123f88451ad6f252c161273";
+	var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&cnt=5&APPID=e3e03c31e123f88451ad6f252c161273&units=imperial";
 
 	// make a request to the url
 	fetch(apiUrl).then(function(response) {
@@ -49,11 +64,46 @@ var displayWeatherData = function(weatherData, searchTerm) {
 	}
 	
 	// clear old content
-	weatherContainerEl.textContent = "";
-	citySearchTerm.textContent = searchTerm;
+	//weatherContainerEl.textContent = "";
+	citySearchTerm.textContent = "5-Day Forecast for " + searchTerm;
 	
-	// Get one set of data to display to start
+	// Get first day set of data to display
+	var weather1Temp = weatherData.list[1].main.temp;
+	var weather1Humidity = weatherData.list[1].main.humidity;
+	var weather1WindSpeed = weatherData.list[1].main.speed;
 	
+	weather1TempEl.textContent = "Temperature: " + weather1Temp + " F";
+	weather1HumidityEl.textContent = "Humidity: " + weather1Humidity + "%";
+	weather1WindEl.textContent = "Wind: " + weather1WindSpeed + " MPH";
+	// Display second day set of data
+	var weather2Temp = weatherData.list[2].main.temp;
+	var weather2Humidity = weatherData.list[2].main.humidity;
+	var weather2WindSpeed = weatherData.list[2].main.speed;
+	
+	weather2TempEl.textContent = "Temperature: " + weather2Temp + " F";
+	weather2HumidityEl.textContent = "Humidity: " + weather2Humidity + "%";
+	weather2WindEl.textContent = "Wind: " + weather2WindSpeed + " MPH";
+	// Display third day set of data	
+	var weather3Temp = weatherData.list[3].main.temp;
+	var weather3Humidity = weatherData.list[3].main.humidity;
+	var weather3WindSpeed = weatherData.list[3].main.speed;
+	
+	weather3TempEl.textContent = "Temperature: " + weather3Temp + " F";
+	weather3HumidityEl.textContent = "Humidity: " + weather3Humidity + "%";
+	weather3WindEl.textContent = "Wind: " + weather3WindSpeed + " MPH";
+	// Display fourth day set of data		
+	var weather4Temp = weatherData.list[4].main.temp;
+	var weather4Humidity = weatherData.list[4].main.humidity;
+	var weather4WindSpeed = weatherData.list[4].main.speed;
+	
+	weather4TempEl.textContent = "Temperature: " + weather4Temp + " F";
+	weather4HumidityEl.textContent = "Humidity: " + weather4Humidity + "%";
+	weather4WindEl.textContent = "Wind: " + weather4WindSpeed + " MPH";
+	
+	console.log(weatherTemp);
+	console.log(weatherHumidity);
+	console.log(weatherWindSpeed);
+	console.log(weatherData);
 	
 }
 
